20ee418fe125633812a7ebce5c4357fff16cfa4d
Matthew Barnes <matthew.simon.barnes@gmail.com>
2017-01-23 14:16:08 -0500

    Made things pretty

---
+++ states/index/index-loggedin.php
@@ -0,0 +1,7 @@
+<?php
+//phpHeader and headerhtml aren't required in here, because this is the
+// 'states' folder, the files in the root directory will already have done this
+?>
+<div class="alert alert-success u-textAlign-center u-alert" style="margin-top: -20px;" role="alert">
+  <span class="alert-link">Hello <?php echo ucfirst("$user"); ?>, you have successfully logged in.</span>
+</div>
