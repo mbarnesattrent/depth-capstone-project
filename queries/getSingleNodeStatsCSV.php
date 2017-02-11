58264a00370ab3a4d4c8292ac7117403c2f3fe8e
Dexter Fichuk <dexterfichuk@gmai.com>
2017-02-11 18:20:06 -0500

    added csv for single node stats

---
+++ queries/getSingleNodeStatsCSV.php
@@ -0,0 +1,29 @@
+<?php
+    //For DB connection
+    require_once "../phpHeader.php";
+
+    //Get the username from the session
+    $user = $_SESSION['user'];
+    
+    // echo $user;
+
+    //Performs a get for the specified node
+    $node = $_GET['nodeID'];
+
+    //Use for testing
+    // $user = "dexterfichuk@gmail.com";
+
+    //Query to get data for the logged in user
+    $sql = "SELECT data.* 
+        FROM data, nodes, users 
+        WHERE users.email = '$user' 
+        AND users.id = nodes.userid
+        AND nodes.nodeID = data.nodeid
+        AND data.nodeid = '$node'";
+    
+    //Print the json so it can be used with a ajax call
+    connectToDb();
+    
+    echo csvQuery($sql);
+    closeConnectToDb();
+?>
