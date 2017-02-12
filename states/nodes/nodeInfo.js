934ae6f60107049454f8dc1fc1e3d6370387e704
Dexter Fichuk <dexterfichuk@gmai.com>
2017-02-11 22:07:12 -0500

    removed some garbage stuff

--- states/nodes/nodeInfo.js
+++ states/nodes/nodeInfo.js
@@ -1,40 +1,8 @@
 
 console.log("About to query");
 
-$.getJSON( "../../queries/getUserNodeData.php?nodeID="+getQueryVariable("nodeID"), function( result ) {
-    
-    console.log(result);
-    console.log("NodeID is " + getQueryVariable("nodeID"));
-     var labels = [],data=[];
+//Put code for chartsright here
 
-         for(var i = 0; i < result.length; i++){
-            //  console.log(result[i]);
-             labels.push(result[i]['timestamp']);
-             data.push(result[i]['waterTemp']);
-          }
-
-    // console.log(labels);
-    var canvas = document.getElementById("myChart");
-    var ctx = canvas.getContext("2d");
-
-    var tempData = {
-        labels : labels,
-        datasets : [{
-            fillColor : "rgba(172,194,132,0.4)",
-            strokeColor : "#ACC26D",
-            pointColor : "#382765",
-            pointStrokeColor : "#9DB86D",
-            data : data
-        }]
-    };
-
-    var myNewChart = new Chart(ctx , {
-        type: "line",
-        data: tempData, 
-    }); 
-    
-
-});
 
 function getQueryVariable(variable)
 {
