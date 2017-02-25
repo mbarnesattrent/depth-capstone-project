5ca6f2ad251beb28512f453c244423e6b88b4440
Dexter Fichuk <dexterfichuk@gmail.com>
2017-02-25 18:09:29 -0500

    Added new map to dashboard,don't need to make new branch as it works fine

--- dashboard.php
+++ dashboard.php
@@ -179,7 +179,7 @@
           <div class='well well-lg'>
             <div class="container"><h1>Map</h1></div>
             <div class="embed-responsive embed-responsive-16by9">
-              <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m24!1m12!1m3!1d45617.77950724317!2d-78.3111359730191!3d44.38983860017785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m9!3e6!4m3!3m2!1d44.3590131!2d-78.2899582!4m3!3m2!1d44.35899!2d-78.289969!5e0!3m2!1sen!2sca!4v1487010690917" style="border:1px solid black;"></iframe>
+              <div id="map-canvas" style="width: 100%; height: 800px"></div>
             </div>
           </div>
           <div class='well well-lg'>
@@ -246,6 +246,7 @@
 <script src="http://dimplejs.org/dist/dimple.v2.3.0.min.js"></script>
 <script src="/states/nodes/dashboardCharts.js"></script>
 <script src="/states/dashboard/dashboard.js"></script>
-
+<script src="http://maps.googleapis.com/maps/api/js?sensor=false&key=AIzaSyAXvuJD0ZBV0a3eK2fXP6CIY82dkLFkRvc"></script> 
+<script src="/states/nodes/map.js"></script>
   </body>
 </html>
